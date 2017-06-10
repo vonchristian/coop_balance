dd96e7548b6ef4d237933d3d80345a4823856b0c
Von Christian Halip <vc.halip@gmail.com>
2017-06-10 16:38:48 +0800

    Update mail gem

---
+++ app/controllers/processing_department/work_in_progress_materials_controller.rb
@@ -0,0 +1,23 @@
+module ProcessingDepartment
+  class WorkInProgressMaterialsController < ApplicationController
+    def new
+      @raw_material = RawMaterial.find(params[:raw_material_id])
+      @work_in_progress_material = WorkInProgressMaterialForm.new
+    end
+    def create
+      @raw_material = RawMaterial.find(params[:raw_material_id])
+      @work_in_progress_material = WorkInProgressMaterialForm.new(work_in_progress_material_params)
+      if @work_in_progress_material.valid?
+        @work_in_progress_material.save
+        redirect_to processing_department_root_url, notice: "Success"
+      else
+        render :new
+      end
+    end
+
+    private
+    def work_in_progress_material_params
+      params.require(:work_in_progress_material_form).permit(:quantity, :date, :raw_material_id)
+    end
+  end
+end
