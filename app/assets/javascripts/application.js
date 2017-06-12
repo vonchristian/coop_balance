2c0f85ae6b4c2174532278652aafb29aa74b6efb
Von Christian Halip <vc.halip@gmail.com>
2017-06-12 11:41:28 +0800

    Fix turbolink issue with adminlte

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,3 +18,13 @@
 //= require bootstrap-datepicker
 //= require icheck
 //= require_tree .
+
+var ready = function () {
+  var o;
+  o = $.AdminLTE.options;
+  if (o.sidebarPushMenu) {
+    $.AdminLTE.pushMenu.activate(o.sidebarToggleSelector);
+  }
+  return $.AdminLTE.layout.activate();
+};
+document.addEventListener('turbolinks:load', ready);
