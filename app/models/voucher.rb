8a45313e3d0e5e96798dc401b372d995ba1177cf
Von Christian Halip <vc.halip@gmail.com>
2017-09-22 22:31:57 +0800

    Add tests for Voucher model

--- app/models/voucher.rb
+++ app/models/voucher.rb
@@ -1,11 +1,12 @@
 class Voucher < ApplicationRecord
   belongs_to :voucherable, polymorphic: true
   belongs_to :payee, polymorphic: true
-  def self.generate_number
+  
+  def self.generate_number_for(voucher)
   	if self.last.present?
-      "#{self.last.number.succ.rjust(12, '0')}"
+      voucher.number = "#{self.last.number.succ.rjust(12, '0')}"
     else 
-      "#{1.to_s.rjust(12, '0')}"
+     voucher.number =  "#{1.to_s.rjust(12, '0')}"
     end
   end
 end
--- spec/models/voucher_spec.rb
+++ spec/models/voucher_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe Voucher, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'associations' do 
+    it { is_expected.to belong_to :voucherable }
+    it { is_expected.to belong_to :payee }
+  end
+  describe ".generate_number" do 
+    it 'for first voucher' do 
+      Voucher.delete_all
+      voucher = Voucher.new
+      Voucher.generate_number_for(voucher)
+      voucher.save
+      expect(voucher.number).to eql('000000000001')
+    end
+    it 'for succeeding voucher' do 
+      first_voucher = Voucher.create(number: '000000000001')
+      second_voucher = Voucher.new
+      Voucher.generate_number_for(second_voucher)
+      second_voucher.save
+
+      expect(second_voucher.number).to eql('000000000002')
+    end
+  end
 end
