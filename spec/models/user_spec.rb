4294060ecf5f2439c01d03666b95972788c2f8d1
Von Christian Halip <vc.halip@gmail.com>
2017-09-25 12:02:37 +0800

    Updates

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'associations' do 
+    it { is_expected.to belong_to :department }
+    it { is_expected.to have_many :entries }
+    it { is_expected.to have_many :appraised_properties }
+  end
 end
