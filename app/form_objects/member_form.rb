bc648dd3e45fe27575c9ed5a95ee344334be28a9
Von Christian Halip <vc.halip@gmail.com>
2017-09-27 10:30:57 +0800

    Update

--- app/form_objects/member_form.rb
+++ app/form_objects/member_form.rb
@@ -1,8 +1,8 @@
-class MemberForm < Reform::Form
-  property :first_name
-  property :middle_name
-  property :last_name
-  property :sex
-  property :date_of_birth
-  validates :first_name, :middle_name, :last_name, :sex, :date_of_birth, presence: true
-end
+# class MemberForm < Reform::Form
+#   property :first_name
+#   property :middle_name
+#   property :last_name
+#   property :sex
+#   property :date_of_birth
+#   validates :first_name, :middle_name, :last_name, :sex, :date_of_birth, presence: true
+# end
