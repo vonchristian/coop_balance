cdf7767c340a1b0d3028bc8587b5c79c9b006b87
Von Christian P. Halip <vc.halip@gmail.com>
2017-10-31 13:40:01 +0800

    Test uploading of avatar of organization

--- spec/system/organizations/create_organization_system_spec.rb
+++ spec/system/organizations/create_organization_system_spec.rb
@@ -8,9 +8,12 @@ describe 'Create Organization', type: :system do
 
   it "with valid attributes" do
     click_link "New Organization"
+    attach_file("uploadOrganizationAvatar", Rails.root + "spec/support/images/default.png")
     fill_in "Name", with: "Women's Organization"
     click_button "Save Organization"
 
     expect(page).to have_content("saved successfully")
+    expect(Organization.count).to eql 1
+    expect(Organization.last.avatar).to be_present
   end
 end
