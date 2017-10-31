1738633c410f96e54ef383d613fb101d130d9076
Von Christian P. Halip <vc.halip@gmail.com>
2017-10-31 11:38:10 +0800

    Implement editing of employee membership

---
+++ spec/system/employees/memberships/edit_employee_membership_system_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+describe 'Edit employee membership' do
+  before(:each) do
+    user = create(:user, role: 'teller')
+    login_as(user, scope: :user)
+    @employee = create(:user)
+    membership = create(:membership, membership_type: 'associate_member', memberable: @employee)
+    visit employee_url(@employee)
+    cash_on_hand = create(:asset, name: "Cash on Hand (Teller)")
+    @employee.cash_on_hand_account = cash_on_hand
+  end
+
+  it 'with valid attributes' do
+    click_link 'Update Membership'
+    choose "Regular Member"
+    click_button "Save Membership"
+
+    expect(page).to have_content("saved successfully")
+    expect(@employee.regular_member?).to be true
+  end
+end
