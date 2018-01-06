9a622334df04cb26d36e4a0ce5c12ee8d3776057
Von Christian P. Halip <vc.halip@gmail.com>
2018-01-06 11:34:54 +0800

    Fix error

--- app/models/invoices/sales_invoice.rb
+++ app/models/invoices/sales_invoice.rb
@@ -1,7 +1,7 @@
 module Invoices
   class SalesInvoice < Invoice
-
-  def self.create_invoice(invoicable)
-    invoicable.create_invoice(number: Invoice.generate_number)
+    def self.create_invoice(invoicable)
+      invoicable.create_invoice(number: Invoice.generate_number)
+    end
   end
 end
