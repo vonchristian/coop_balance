71bf611b47a3abde887effa423e866c9e4ded450
Von Christian P. Halip <vc.halip@gmail.com>
2018-01-16 10:23:14 +0800

    Add unit test to amortization schedule

--- app/models/loans_module/amortization_schedule.rb
+++ app/models/loans_module/amortization_schedule.rb
@@ -5,45 +5,10 @@ module LoansModule
     has_many :notes, as: :noteable
 
     accepts_nested_attributes_for :notes
-    def self.create_schedule_for(loan)
-      loan.amortization_schedules.destroy_all
-      if loan.monthly? || loan.quarterly? || loan.semi_annually?
-        create_amort_schedule(loan)
-      elsif loan.lumpsum?
-        create_lumpsum_amortization_schedule(loan)
-      end
-    end
 
-    def self.starting_date(loan)
-      if loan.disbursed?
-        loan.disbursement_date
-      else
-        loan.application_date
-      end
-    end
-
-    def self.create_amort_schedule(loan)
-      first_amortization = loan.amortization_schedules.create(
-				date: first_amortization_date_for(loan),
-				principal: principal_amount_for(loan),
-				interest:interest_amount_for(loan)
-			)
-      ActiveRecord::Base.transaction do
-        number_of_schedules_for(loan).times do
-          loan.amortization_schedules.create(
-						date: schedule_date_for(loan),
-						principal: principal_amount_for(loan),
-						interest: interest_amount_for(loan)
-					)
-        end
-      end
-    end
-
-    def self.create_lumpsum_amortization_schedule(loan)
-      loan.amortization_schedules.create(
-				date: starting_date(loan) + loan.term.to_i.months,
-				principal: (loan.loan_amount), interest:
-				loan.interest_on_loan_balance)
+		def self.create_schedule_for(loan)
+			create_first_schedule(loan)
+      create_succeeding_schedule(loan)
     end
 
     def self.for(options={})
@@ -73,6 +38,38 @@ module LoansModule
       loan.loan_charge_payment_schedules.scheduled_for(date).sum(:amount)
     end
 
+
+
+		private
+		def self.starting_date(loan)
+      if loan.disbursed?
+        loan.disbursement_date
+      else
+        loan.application_date
+      end
+    end
+
+		def self.create_first_schedule(loan)
+			first_amortization = loan.amortization_schedules.create(
+				date: first_amortization_date_for(loan),
+				principal: principal_amount_for(loan),
+				interest:interest_amount_for(loan)
+			)
+		end
+
+		def self.create_succeeding_schedule(loan)
+			if !loan.lumpsum?
+				ActiveRecord::Base.transaction do
+					number_of_schedules_for(loan).times do
+						loan.amortization_schedules.create(
+							date: schedule_date_for(loan),
+							principal: principal_amount_for(loan),
+							interest: interest_amount_for(loan)
+						)
+					end
+				end
+			end
+		end
 		def self.principal_amount_for(loan)
 			if loan.quarterly?
 			  (loan.loan_amount / (loan.term / 4))
@@ -80,6 +77,8 @@ module LoansModule
 				(loan.loan_amount / loan.term.to_i)
 			elsif loan.semi_annually?
 				(loan.loan_amount / (loan.term.to_i/6))
+			elsif loan.lumpsum?
+				(loan.loan_amount)
 			end
 		end
 
@@ -90,6 +89,8 @@ module LoansModule
 			  (loan.interest_on_loan_balance / (loan.term/4))
 			elsif loan.semi_annually?
 				(loan.interest_on_loan_balance/(loan.term.to_i/6))
+			elsif loan.lumpsum?
+				loan.interest_on_loan_balance
 			end
 		end
 
@@ -119,6 +120,8 @@ module LoansModule
 				starting_date(loan).next_quarter
 			elsif loan.semi_annually?
 				starting_date(loan).next_quarter.next_quarter
+			elsif loan.lumpsum?
+				starting_date(loan) + loan.term.to_i.months
 			end
 		end
 	end
--- spec/models/loans_module/amortization_schedule_spec.rb
+++ spec/models/loans_module/amortization_schedule_spec.rb
@@ -7,6 +7,28 @@ module LoansModule
     	it { is_expected.to belong_to :loan }
       it { is_expected.to have_many :notes }
     end
-    it
+
+    describe '.create_schedule_for(loan)' do
+      it 'monthly' do
+        monthly_loan = create(:loan, mode_of_payment: 'monthly', term: 36, application_date: Date.today)
+        LoansModule::AmortizationSchedule.create_schedule_for(monthly_loan)
+
+        expect(monthly_loan.amortization_schedules.count).to eql 36
+      end
+      
+      it 'quarterly' do
+        quarterly_loan = create(:loan, mode_of_payment: 'quarterly', term: 36, application_date: Date.today)
+        LoansModule::AmortizationSchedule.create_schedule_for(quarterly_loan)
+
+        expect(quarterly_loan.amortization_schedules.count).to eql 9
+      end
+
+      it 'semi_annually' do
+        semi_annually_loan = create(:loan, mode_of_payment: 'semi_annually', term: 36, application_date: Date.today)
+        LoansModule::AmortizationSchedule.create_schedule_for(semi_annually_loan)
+
+        expect(semi_annually_loan.amortization_schedules.count).to eql 6
+      end
+    end
   end
 end
