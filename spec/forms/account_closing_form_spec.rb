4660c33ce3c6f7d433cc34a68b2ad865dae61f20
Von Christian P. Halip <vc.halip@gmail.com>
2018-01-17 09:30:09 +0800

    Update

--- spec/forms/account_closing_form_spec.rb
+++
@@ -1,8 +0,0 @@
-require 'rails_helper'
-
-describe AccountClosingForm, type: :model do
-  describe 'amount' do
-    it { is_expected.to validate_presence_of :amount }
-    it { is_expected.to validate_numericality_of :amount }
-  end
-end
--- spec/forms/bank_account_form_spec.rb
+++
@@ -1,8 +0,0 @@
-require 'rails_helper'
-
-describe BankAccountForm, type: :model do
-  describe 'validations' do
-    it { is_expected.to validate_presence_of :account_id }
-    it { is_expected.to validate_presence_of :earned_interest_account_id }
-  end
-end
