c7def233f25619bc8874eb0fee7820cd23a9bd27
Von Christian P. Halip <vc.halip@gmail.com>
2018-02-06 20:54:04 +0800

    Add unit tests to purchase order model

---
+++ spec/models/store_front_module/orders/purchase_order_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+module StoreFrontModule
+  module Orders
+    describe PurchaseOrder do
+      describe 'associations' do
+        it { is_expected.to have_one :voucher }
+        it { is_expected.to have_many :purchase_order_line_items }
+        it { is_expected.to have_many :products }
+      end
+      describe 'delegations' do
+        it { is_expected.to delegate_method(:number).to(:voucher).with_prefix }
+        it { is_expected.to delegate_method(:date).to(:voucher).with_prefix }
+        it { is_expected.to delegate_method(:disburser_full_name).to(:voucher).with_prefix }
+        it { is_expected.to delegate_method(:name).to(:supplier).with_prefix }
+      end
+    end
+  end
+end
