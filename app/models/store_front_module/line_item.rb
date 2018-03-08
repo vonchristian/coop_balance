d49d347270ebbb222e0bf696e31d3442cc25c22f
Von Christian Halip <vc.halip@gmail.com>
2018-03-08 20:47:45 +0800

    Add unit tests to LineItem model

--- app/models/store_front_module/line_item.rb
+++ app/models/store_front_module/line_item.rb
@@ -4,15 +4,22 @@ module StoreFrontModule
     include PgSearch
     pg_search_scope :text_search, against: [:barcode]
     belongs_to :unit_of_measurement, class_name: "StoreFrontModule::UnitOfMeasurement"
-    belongs_to :cart, class_name: "StoreFrontModule::Cart"
-    belongs_to :product, touch: true
+    belongs_to :cart,                class_name: "StoreFrontModule::Cart"
+    belongs_to :product,             class_name: "StoreFrontModule::Product",
+                                     touch: true
+    belongs_to :order,               class_name: "StoreFrontModule::Order",
+                                     foreign_key: 'order_id'
 
-    validates :unit_of_measurement_id, :product_id, presence: true
-    delegate :name, to: :product
-    delegate :code, to: :unit_of_measurement, prefix: true
-    delegate :conversion_multiplier, to: :unit_of_measurement
-    delegate :balance, to: :product, prefix: true
-
+    validates :unit_of_measurement_id,
+              :product_id,
+              presence: true
+    delegate :name,                   to: :product
+    delegate :code,                   to: :unit_of_measurement,
+                                      prefix: true
+    delegate :conversion_multiplier,  to: :unit_of_measurement
+    delegate :balance,                to: :product, prefix: true
+    delegate :employee,               to: :order
+    delegate :name,                   to: :employee, prefix: true
 
     def self.total
       all.sum(&:converted_quantity)
--- spec/models/store_front_module/line_item_spec.rb
+++ spec/models/store_front_module/line_item_spec.rb
@@ -6,15 +6,21 @@ module StoreFrontModule
     	it { is_expected.to belong_to :cart }
       it { is_expected.to belong_to :unit_of_measurement }
       it { is_expected.to belong_to :product }
+      it { is_expected.to belong_to :order }
     end
-
     describe 'validations' do
       it { is_expected.to validate_presence_of :unit_of_measurement_id }
       it { is_expected.to validate_presence_of :product_id }
     end
-
-
     describe 'delegations' do
+      it { is_expected.to delegate_method(:balance).to(:product).with_prefix }
+      it { is_expected.to delegate_method(:conversion_multiplier).to(:unit_of_measurement) }
+      it { is_expected.to delegate_method(:code).to(:unit_of_measurement).with_prefix }
+      it { is_expected.to delegate_method(:name).to(:product) }
+      it { is_expected.to delegate_method(:employee).to(:order) }
+      it { is_expected.to delegate_method(:name).to(:employee).with_prefix }
+
+
     end
 
     it ".total" do
