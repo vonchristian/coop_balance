42a6a50fb013258693d305f7eae41ae3e889686c
Von Christian Halip <vc.halip@gmail.com>
2018-08-03 17:13:53 +0800

    Fix

--- db/migrate/20170610070632_add_has_freight_to_raw_material_stocks.rb
+++
@@ -1,8 +0,0 @@
-class AddHasFreightToRawMaterialStocks < ActiveRecord::Migration[5.1]
-  def change
-    add_column :raw_material_stocks, :has_freight, :boolean, default: false
-    add_column :raw_material_stocks, :discounted, :boolean, default: false
-    add_column :raw_material_stocks, :freight_in, :decimal, default: 0
-    add_column :raw_material_stocks, :discount_amount, :decimal, default: 0
-  end
-end
