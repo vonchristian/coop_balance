2f769f71ce23336ddc507a024e9fba082a021acf
Von Christian Halip <vc.halip@gmail.com>
2018-08-03 17:22:30 +0800

    Fix

--- db/migrate/20180207063251_add_sales_return_account_to_store_front_configs.rb
+++
@@ -1,5 +0,0 @@
-class AddSalesReturnAccountToStoreFrontConfigs < ActiveRecord::Migration[5.1]
-  def change
-    add_reference :store_front_configs, :sales_return_account, foreign_key: { to_table: :accounts }, type: :uuid
-  end
-end
--- db/migrate/20180207111918_add_accounts_payable_account_to_store_front_configs.rb
+++
@@ -1,5 +0,0 @@
-class AddAccountsPayableAccountToStoreFrontConfigs < ActiveRecord::Migration[5.1]
-  def change
-    add_reference :store_front_configs, :accounts_payable_account, foreign_key: { to_table: :accounts }, type: :uuid
-  end
-end
