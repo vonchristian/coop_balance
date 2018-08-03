70636a25a62da1a3260e5055521eaf8ff3396002
Von Christian Halip <vc.halip@gmail.com>
2018-08-03 17:15:19 +0800

    Fix migrations

--- db/migrate/20170610085049_add_product_to_finished_goods_materials.rb
+++
@@ -1,5 +0,0 @@
-class AddProductToFinishedGoodsMaterials < ActiveRecord::Migration[5.1]
-  def change
-    add_reference :finished_good_materials, :product, foreign_key: true, type: :uuid
-  end
-end
--- db/migrate/20170712113907_add_committee_to_committee_members.rb
+++
@@ -1,5 +0,0 @@
-class AddCommitteeToCommitteeMembers < ActiveRecord::Migration[5.1]
-  def change
-    add_reference :committee_members, :committee, foreign_key: true, type: :uuid
-  end
-end
