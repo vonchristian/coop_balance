78d20ab6156f713134c1edd2a0a51ab079e2d8ae
Von Christian Halip <vc.halip@gmail.com>
2018-09-12 06:10:19 +0800

    Update bank account page

--- app/models/bank_account.rb
+++ app/models/bank_account.rb
@@ -1,4 +1,5 @@
 class BankAccount < ApplicationRecord
+  has_one_attached :avatar
   belongs_to :cooperative
   belongs_to :account,                 class_name: "AccountingModule::Account"
   belongs_to :earned_interest_account, class_name: "AccountingModule::Account"
@@ -6,16 +7,7 @@ class BankAccount < ApplicationRecord
   validates :bank_name, :bank_address, :account_number, presence: true
   validates :account_id, :earned_interest_account_id, presence: true
   has_many :entries, class_name: "AccountingModule::Entry", as: :commercial_document, dependent: :destroy
-  has_attached_file :avatar,
-  styles: { large: "120x120>",
-           medium: "70x70>",
-           thumb: "40x40>",
-           small: "30x30>",
-           x_small: "20x20>"},
-  default_url: ":style/profile_default.jpg",
-  :path => ":rails_root/public/system/:attachment/:id/:basename_:style.:extension",
-  :url => "/system/:attachment/:id/:basename_:style.:extension"
-  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
+  before_save :set_default_image
   def balance
    account.balance(commercial_document: self)
   end
@@ -30,5 +22,11 @@ class BankAccount < ApplicationRecord
 
   def earned_interests
     earned_interest_account.balance(commercial_document: self)
+  end
+  private
+  def set_default_image
+    if avatar.attachment.blank?
+      self.avatar.attach(io: File.open(Rails.root.join('app', 'assets', 'images', 'bank.png')), filename: 'default-image.png', content_type: 'image/png')
+    end
   end
 end
