a0475b714855549522d078c18d4687c64100de28
Von Christian Halip <vc.halip@gmail.com>
2018-10-31 09:01:02 +0800

    Update pages

--- app/controllers/time_deposit_applications/voucher_confirmations_controller.rb
+++ app/controllers/time_deposit_applications/voucher_confirmations_controller.rb
@@ -5,7 +5,7 @@ module TimeDepositApplications
       @voucher = Voucher.find(params[:voucher_id])
       ActiveRecord::Base.transaction do
         TimeDeposits::Opening.new(time_deposit_application: @time_deposit_application, employee: current_user, voucher: @voucher).process!
-        Vouchers::EntryProcessing.new(voucher_id: @voucher.id, employee_id: current_user.id).process!
+        Vouchers::EntryProcessing.new(voucher: @voucher, employee: current_user).process!
         redirect_to vouchers_url, notice: "confirmed successfully."
       end
     end
