a056083681c700a918ca117ca0259d37a1bdc5b2
Von Christian Halip <vc.halip@gmail.com>
2018-11-06 08:13:19 +0800

    Updats

--- app/models/vouchers/amount_adjustment.rb
+++ app/models/vouchers/amount_adjustment.rb
@@ -2,5 +2,14 @@ module Vouchers
   class AmountAdjustment < ApplicationRecord
     belongs_to :loan_application, class_name: "LoansModule::LoanApplication"
     has_many :voucher_amounts, class_name: "Vouchers::VoucherAmount", dependent: :destroy
+    def adjusted_amount
+      if number_of_payments && number_of_payments > 0
+        loan_application.amortization_schedules.order(date: :asc).first(number_of_payments).sum(&:interest)
+      elsif percent
+        amount * (percent / 100.0)
+      else
+        amount
+      end
+    end
   end
 end
--- app/models/vouchers/voucher_amount.rb
+++ app/models/vouchers/voucher_amount.rb
@@ -5,15 +5,18 @@ module Vouchers
     belongs_to :voucher
     belongs_to :recorder, class_name: "User", foreign_key: 'recorder_id'
     belongs_to :commercial_document, polymorphic: true
-    delegate :name, to: :account, prefix: true
     belongs_to :amount_adjustment, class_name: "Vouchers::AmountAdjustment"
-    validates  :amount, :account_id,  :amount_type, presence: true
+
+    delegate :name, to: :account, prefix: true
+
+    validates :amount, :account_id, :amount_type, presence: true
     validates :amount, numericality: true
 
 
     def self.total
       sum(:amount)
     end
+
     def self.for_account(args={})
       where(account: args[:account])
     end
@@ -30,17 +33,16 @@ module Vouchers
     def self.with_no_vouchers
       select{ |a| a.voucher.nil? }
     end
+
     def adjusted_amount
-      if amount_adjustment && amount_adjustment.amount
-        amount - amount_adjustment.amount
-      elsif amount_adjustment && amount_adjustment.number_of_payments && amount_adjustment.number_of_payments > 0
-        loan_application.amortization_schedules.order(date: :asc).first(number_of_payments).sum(&:interest)
-      elsif amount_adjustment && amount_adjustment.percent
-        amount * (amount_adjustment.percent / 100.0)
+      if amount_adjustment
+        amount_adjustment.adjusted_amount
       else
         amount
       end
     end
+    
+
     def self.balance_for_new_record
       balance = BigDecimal.new('0')
       self.all.each do |amount_record|
