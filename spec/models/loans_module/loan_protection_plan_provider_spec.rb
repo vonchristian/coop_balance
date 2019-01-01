d26452c1b36277ee78bb83ad241ed998828c20f6
Von Christian Halip <vc.halip@gmail.com>
2019-01-01 10:08:12 +0800

    Fix error

--- spec/models/loans_module/loan_protection_plan_provider_spec.rb
+++ spec/models/loans_module/loan_protection_plan_provider_spec.rb
@@ -8,15 +8,16 @@ module LoansModule
       it { is_expected.to have_many :loan_products }
       it { is_expected.to have_many :loans }
     end
+
     describe 'validations' do
       it { is_expected.to validate_presence_of :business_name }
       it { is_expected.to validate_uniqueness_of(:business_name).scoped_to(:cooperative_id) }
       it { is_expected.to validate_presence_of :rate }
       it { is_expected.to validate_numericality_of :rate }
     end
+
     describe 'delegations' do
       it { is_expected.to delegate_method(:name).to(:accounts_payable).with_prefix }
-    end
     end
   end
 end
