c472c800207251fed629fe61a9fc99d1eda7408e
Von Christian Halip <vc.halip@gmail.com>
2019-01-19 09:15:47 +0800

    Fix stale migrations

--- db/migrate/20190114072752_update_blob_on_active_storage.rb
+++
@@ -1,6 +0,0 @@
-class UpdateBlobOnActiveStorage < ActiveRecord::Migration[5.2]
-  def change
-  	remove_reference :active_storage_attachments, :blob
-  	add_reference    :active_storage_attachments, :blob, type: :uuid
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -54,12 +54,12 @@ ActiveRecord::Schema.define(version: 2019_01_18_232002) do
 
   create_table "active_storage_attachments", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
     t.string "name", null: false
+    t.string "record_type", null: false
+    t.uuid "record_id", null: false
+    t.uuid "blob_id", null: false
     t.datetime "created_at", null: false
-    t.string "record_type"
-    t.uuid "record_id"
-    t.uuid "blob_id"
     t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
-    t.index ["record_type", "record_id"], name: "index_active_storage_attachments_on_record_type_and_record_id"
+    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
   end
 
   create_table "active_storage_blobs", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
@@ -1191,8 +1191,8 @@ ActiveRecord::Schema.define(version: 2019_01_18_232002) do
     t.string "account_number"
     t.datetime "date_opened"
     t.string "account_owner_name"
-    t.datetime "created_at", default: "2019-01-19 01:11:33", null: false
-    t.datetime "updated_at", default: "2019-01-19 01:11:33", null: false
+    t.datetime "created_at", default: "2019-01-19 01:13:39", null: false
+    t.datetime "updated_at", default: "2019-01-19 01:13:39", null: false
     t.integer "status"
     t.uuid "office_id"
     t.string "subscriber_type"
