fda09c960571cc6b5bb21e6cc53fee26935efb2d
Christopher Pumihic <tophie.pumihic@gmail.com>
2019-02-01 12:04:27 +0800

    fixes

--- app/forms/loans_module/loan_applications/capital_build_up_processing.rb
+++ app/forms/loans_module/loan_applications/capital_build_up_processing.rb
@@ -17,7 +17,7 @@ module LoansModule
         find_loan_application.voucher_amounts.create!(
         description:         "Capital Build Up",
         amount:              amount,
-        account:             find_share_capital.share_capital_product_paid_up_account,
+        account:             find_share_capital.share_capital_product_equity_account,
         commercial_document: find_share_capital,
         cooperative: find_loan_application.cooperative,
         amount_type: 'credit')
