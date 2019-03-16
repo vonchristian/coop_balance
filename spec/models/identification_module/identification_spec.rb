d48d581a492de0cf5698343707a5c62fcb70aaf6
Von Christian <vc.halip@gmail.com>
2019-03-16 21:14:43 +0800

    adding identifications of member

--- spec/models/identification_module/identification_spec.rb
+++ spec/models/identification_module/identification_spec.rb
@@ -6,5 +6,9 @@ module IdentificationModule
       it { is_expected.to belong_to :identity_provider }
       it { is_expected.to belong_to :identifiable }
     end
+
+    describe 'delegations' do
+      it { is_expected.to delegate_method(:name).to(:identity_provider).with_prefix }
+    end
   end
 end
--- spec/models/identification_module/identity_provider_spec.rb
+++ spec/models/identification_module/identity_provider_spec.rb
@@ -6,11 +6,13 @@ module IdentificationModule
       it { is_expected.to have_many :issued_identities }
     end
     describe 'validations' do
-      it { is_expected.to validate_presence_of :name }
-      it { is_expected.to validate_presence_of :abbreviated_name }
+      it { is_expected.to validate_presence_of   :name }
+      it { is_expected.to validate_presence_of   :abbreviated_name }
+      it { is_expected.to validate_presence_of   :account_number }
       it { is_expected.to validate_uniqueness_of :name }
       it { is_expected.to validate_uniqueness_of :abbreviated_name }
-
+      it { is_expected.to validate_uniqueness_of :account_number }
     end
+    
   end
 end
