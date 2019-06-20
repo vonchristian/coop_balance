5793dbc5f7d263ecea5bb0aa8968a74babc42e11
Von Christian <vc.halip@gmail.com>
2019-06-20 16:07:35 +0800

    updates

--- app/models/accounting_module/account_category.rb
+++ app/models/accounting_module/account_category.rb
@@ -1,10 +1,18 @@
+#use to consolidate accounts into a single category
 module AccountingModule
   class AccountCategory < ApplicationRecord
     enum category_type: [:asset, :liability, :equity, :revenue, :expense]
-
     belongs_to :cooperative
-    has_many :account_sub_categories, class_name: 'AccountingModule::AccountSubCategory', foreign_key: 'main_category_id'
-    has_many :sub_categories,         class_name: 'AccountingModule::AccountCategory', through: :account_sub_categories, source: :sub_category
-    has_many :accounts, class_name: 'AccountingModule::Account', dependent: :nullify
+    has_many :account_sub_categories,  class_name: 'AccountingModule::AccountSubCategory', foreign_key: 'main_category_id'
+    has_many :sub_categories,          through: :account_sub_categories, source: :sub_category
+    has_many :accounts,                class_name: 'AccountingModule::Account', dependent: :nullify
+
+    validates :title, presence: true, uniqueness: { scope: :cooperative_id }
+
+    delegate :name, to: :cooperative, prefix: true
+
+    def title_and_cooperative_name
+      "#{title} (#{cooperative_name})"
+    end
   end
 end
--- app/models/accounting_module/account_sub_category.rb
+++ app/models/accounting_module/account_sub_category.rb
@@ -1,7 +1,8 @@
 module AccountingModule
   class AccountSubCategory < ApplicationRecord
-    belongs_to :main_category, class_name: 'AccountingModule::AccountCategory'
-    belongs_to :sub_category, class_name: 'AccountingModule::AccountCategory'
+
+    belongs_to :main_category, class_name: 'AccountingModule::AccountCategory', foreign_key: 'main_category_id'
+    belongs_to :sub_category, class_name: 'AccountingModule::AccountCategory', foreign_key: 'sub_category_id'
     has_many :accounts,           class_name: 'AccountingModule::Account'
 
   end
