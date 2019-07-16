7cb6c7b1c3c60502d49280bb084d0a8ecd60274b
Von Christian <vc.halip@gmail.com>
2019-07-16 11:59:42 +0800

    updates

--- app/controllers/portfolios/savings_controller.rb
+++ app/controllers/portfolios/savings_controller.rb
@@ -4,9 +4,9 @@ module Portfolios
 		def index
 			if params[:to_date].present?
 				@to_date = params[:to_date].present? ? Date.parse(params[:to_date]) : Time.now
-      	@savings_accounts = current_office.savings.order(:account_owner_name)
+      	@savings_accounts = MembershipsModule::Saving.all.order(:account_owner_name)
       else
-      	@savings_accounts = current_office.savings
+      	@savings_accounts = MembershipsModule::Saving.all
       end
       respond_to do |format|
 	      format.html
