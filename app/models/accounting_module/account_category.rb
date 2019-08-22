0e72f9e363af939622719cbe565477924c9ac4af
Von Christian <vc.halip@gmail.com>
2019-08-22 08:17:27 +0800

    fix income statement

--- app/models/accounting_module/account_category.rb
+++ app/models/accounting_module/account_category.rb
@@ -1,25 +1,54 @@
 #use to consolidate accounts into a single category
 module AccountingModule
   class AccountCategory < ApplicationRecord
+    class_attribute :normal_credit_balance
     extend AccountingModule::UpdatedAtFinder
-    enum category_type: [:asset, :liability, :equity, :revenue, :expense]
+    include PgSearch
+    pg_search_scope :text_search, against: [:title, :code]
+
     belongs_to :cooperative
+    belongs_to :office, class_name: 'Cooperatives::Office'
     has_many :account_sub_categories,  class_name: 'AccountingModule::AccountSubCategory', foreign_key: 'main_category_id'
     has_many :sub_categories,          through: :account_sub_categories, source: :sub_category
     has_many :accounts,                class_name: 'AccountingModule::Account', dependent: :nullify
-    has_many :entries,                 through: :accounts
-    has_many :debit_entries,                 through: :accounts
-    has_many :credit_entries,                 through: :accounts
-
+    has_many :amounts,                 through: :accounts, class_name: 'AccountingModule::Amount'
+    has_many :debit_amounts,           through: :accounts, class_name: 'AccountingModule::DebitAmount'
+    has_many :credit_amounts,          through: :accounts, class_name: 'AccountingModule::CreditAmount'
+    has_many :entries,                 through: :accounts, class_name: 'AccountingModule::Entry'
+    has_many :debit_entries,           through: :accounts, class_name: 'AccountingModule::Entry'
+    has_many :credit_entries,          through: :accounts, class_name: 'AccountingModule::Entry'
 
     validates :title, :code, presence: true, uniqueness: { scope: :cooperative_id }
-    validates :category_type, presence: true
+    validates :type, presence: true
     delegate :name, to: :cooperative, prefix: true
 
+    scope :assets,      -> { where(type: 'AccountingModule::AccountCategories::Asset') }
+    scope :liabilities, -> { where(type: 'AccountingModule::AccountCategories::Liability') }
+    scope :equities,    -> { where(type: 'AccountingModule::AccountCategories::Equity') }
+    scope :revenues,    -> { where(type: 'AccountingModule::AccountCategories::Revenue') }
+    scope :expenses,    -> { where(type: 'AccountingModule::AccountCategories::Expense') }
+
+
+    def self.types
+      ["AccountingModule::AccountCategories::Asset",
+       "AccountingModule::AccountCategories::Equity",
+       "AccountingModule::AccountCategories::Liability",
+       "AccountingModule::AccountCategories::Expense",
+       "AccountingModule::AccountCategories::Revenue"]
+    end
+
+    def self.main_categories
+      ids = AccountingModule::AccountSubCategory.main_category_ids + self.ids
+      where(id: ids.uniq.compact.flatten).
+      where.not(id: AccountingModule::AccountSubCategory.sub_category_ids)
+    end
 
     def self.without_sub_categories
-      where.not(id: AccountingModule::AccountSubCategory.main_categories.ids)
+      ids = AccountingModule::AccountSubCategory.sub_category_ids + self.ids
+      where(id: ids.uniq.compact.flatten).
+      where.not(id: AccountingModule::AccountSubCategory.main_category_ids)
     end
+
 
     def all_accounts
       account_ids = []
@@ -28,69 +57,82 @@ module AccountingModule
       AccountingModule::Account.where(id: account_ids.uniq.compact.flatten)
     end
 
-    def self.main_categories
-      where(id: AccountingModule::AccountSubCategory.main_categories.ids).
-      where.not(id: AccountingModule::AccountSubCategory.sub_categories.ids)
-    end
-
     def self.balance(args={})
       accounts_balance = BigDecimal('0')
-      self.all.each do |category|
-        accounts_balance += category.balance(args)
+      without_sub_categories.each do |category|
+        if category.contra?
+          accounts_balance -= category.balance(args)
+        else
+          accounts_balance += category.balance(args)
+        end
       end
       accounts_balance
     end
 
+
     def self.debits_balance(args={})
       accounts_balance = BigDecimal('0')
-      self.all.each do |category|
-        accounts_balance += category.debits_balance(args)
+      without_sub_categories.each do |category|
+        if category.contra?
+          accounts_balance -= category.debits_balance(args)
+        else
+          accounts_balance += category.debits_balance(args)
+        end
       end
       accounts_balance
     end
 
     def self.credits_balance(args={})
       accounts_balance = BigDecimal('0')
-      self.all.each do |category|
-        accounts_balance += category.credits_balance(args)
+      self.all.each do |main_category|
+        if main_category.sub_categories.exists?
+          main_category.sub_categories.each do |category|
+            if category.contra?
+              accounts_balance -= category.credits_balance(args)
+            else
+              accounts_balance += category.credits_balance(args)
+            end
+          end
+        end
       end
       accounts_balance
     end
 
     def balance(args={})
-      accounts_balance ||= BigDecimal('0')
-      if sub_categories.exists?
-        sub_categories.balance(args)
+      return raise(NoMethodError, "undefined method 'balance'") if self.class == AccountingModule::AccountCategory
+      if self.normal_credit_balance ^ contra
+        credits_balance(args) - debits_balance(args)
       else
-        accounts.includes(:amounts => :entry).each do |account|
-          accounts_balance += account.balance(args)
-        end
-        accounts_balance
-      end
-    end
-
-    def debits_balance(args={})
-      if sub_categories.exists?
-        sub_categories.debits_balance(args)
-      else
-        accounts_balance = BigDecimal('0')
-        accounts.includes(:debit_amounts =>:entry).each do |account|
-          accounts_balance += account.debits_balance(args)
-        end
-        accounts_balance
+        debits_balance(args) - credits_balance(args)
       end
     end
 
     def credits_balance(args={})
+      accounts_balance = BigDecimal('0')
       if sub_categories.exists?
-        sub_categories.credits_balance(args)
-      else
-        accounts_balance = BigDecimal('0')
-        accounts.includes(:debit_amounts =>:entry).each do |account|
-          accounts_balance += account.credits_balance(args)
+        sub_categories.each do |category|
+          accounts_balance += category.credits_balance(args)
         end
-        accounts_balance
+      else
+        accounts_balance += credit_amounts.balance(args)
       end
+      accounts_balance
+    end
+
+    def debits_balance(args={})
+      accounts_balance = BigDecimal('0')
+      if sub_categories.exists?
+        sub_categories.each do |category|
+          accounts_balance += category.debits_balance(args)
+        end
+      else
+        accounts_balance += debit_amounts.balance(args)
+      end
+      accounts_balance
+    end
+
+    def normalized_type
+      type.gsub("AccountingModule::AccountCategories::", "")
     end
   end
 end
--- app/models/accounting_module/account_sub_category.rb
+++ app/models/accounting_module/account_sub_category.rb
@@ -1,17 +1,26 @@
 module AccountingModule
   class AccountSubCategory < ApplicationRecord
-    belongs_to :main_category, class_name: 'AccountingModule::AccountCategory', foreign_key: 'main_category_id'
-    belongs_to :sub_category,  class_name: 'AccountingModule::AccountCategory', foreign_key: 'sub_category_id'
-    validates :sub_category_id, uniqueness: { scope: :main_category_id }
+    belongs_to :main_category,  class_name: 'AccountingModule::AccountCategory', foreign_key: 'main_category_id'
+    belongs_to :sub_category,   class_name: 'AccountingModule::AccountCategory', foreign_key: 'sub_category_id'
 
-    def self.main_categories
-      ids = pluck(:main_category_id)
-      AccountingModule::AccountCategory.where(id: ids)
+    validates :sub_category_id, uniqueness: { scope: :main_category_id }
+    validate :same_origin?
+    validate :same_type?
+
+    def self.main_category_ids
+      pluck(:main_category_id)
     end
 
-    def self.sub_categories
-      ids = pluck(:sub_category_id)
-      AccountingModule::AccountCategory.where(id: ids)
+    def self.sub_category_ids
+      pluck(:sub_category_id)
+    end
+
+    private
+    def same_origin?
+      errors[:sub_category_id] << "Not the same origin" if self.main_category.cooperative != self.sub_category.cooperative
+    end
+    def same_type?
+      errors[:sub_category_id] << "Not the same type" if self.main_category.type != self.sub_category.type
     end
   end
 end
