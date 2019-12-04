1669c8e3552a13966b8d1599c88074f5fe9ca12d
Von Christian <vc.halip@gmail.com>
2019-12-04 11:27:47 +0800

    Fix savings accounts test

---
+++ spec/system/savings_accounts/balance_transfers/new_system_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+describe 'New balance transfer' do
+  it 'with valid attributes' do
+  end
+
+  it 'with invalid' do
+  end
+end 
--- spec/system/savings_accounts/withdrawals/new_system_spec.rb
+++ spec/system/savings_accounts/withdrawals/new_system_spec.rb
@@ -3,31 +3,34 @@ require 'rails_helper'
 describe 'New savings account withdrawal' do
   before(:each) do
     cash_on_hand_account = create(:asset, name: "Cash on Hand (Teller)")
-    user = create(:user, role: 'teller', cash_on_hand_account: cash_on_hand_account)
+    user                 = create(:user, role: 'teller')
+    user.cash_accounts << cash_on_hand_account
+    savings_account = create(:saving, cooperative: user.cooperative)
     login_as(user, scope: :user )
-    savings_account = create(:saving)
 
     visit savings_account_url(savings_account)
-    click_link "New Deposit"
+    click_link "Deposit"
     fill_in "Amount", with: 100_000
-    fill_in 'OR number', with: '909045'
+    fill_in 'Reference Number', with: '909045'
     fill_in 'Date', with: Date.today
 
-    click_button "Save Deposit"
-    click_link "New Withdraw"
+    click_button "Proceed"
+    click_link "Confirm Transaction"
+    click_link 'Withdraw'
   end
   it 'with valid attributes' do
     fill_in "Amount", with: 10_000
-    fill_in 'OR number', with: '909045'
+    fill_in 'Reference Number', with: '909045'
     fill_in 'Date', with: Date.today
 
-    click_button "Save Withdraw"
+    click_button "Proceed"
 
-    expect(page).to have_content('saved successfully')
+    expect(page).to have_content('created successfully')
+    click_link 'Confirm Transaction'
   end
 
   it 'with invalid attributes' do
-    click_button "Save Withdraw"
+    click_button "Proceed"
 
     expect(page).to have_content("can't be blank")
   end
