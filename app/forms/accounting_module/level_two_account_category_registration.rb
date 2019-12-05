15789e4ceba257bc7d866ed84adbaffd54b53080
Von Christian <vc.halip@gmail.com>
2019-12-05 11:27:35 +0800

    feat: new level two account category

---
+++ app/forms/accounting_module/level_two_account_category_registration.rb
@@ -0,0 +1,35 @@
+module AccountingModule
+  class LevelTwoAccountCategoryRegistration
+    include ActiveModel::Model
+    
+    attr_accessor :title, :code, :contra, :type, :office_id
+
+    validates :title, :code, :type, :office_id, presence: true
+    validates :type, inclusion: { in: AccountingModule::Account::TYPES }
+    def register!
+      if valid?
+        ActiveRecord::Base.transaction do
+          create_level_two_account_category
+        end
+      end
+    end
+
+    def normalized_type
+      ("AccountingModule::AccountCategories::LevelTwoAccountCategories::#{type}")
+    end
+
+    private
+    def create_level_two_account_category
+      find_office.level_two_account_categories.create!(
+        title:  title,
+        code:   code,
+        type:   normalized_type,
+        contra: contra
+      )
+    end
+
+    def find_office
+      Cooperatives::Office.find(office_id)
+    end
+  end
+end
