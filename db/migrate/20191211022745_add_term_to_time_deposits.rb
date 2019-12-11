7d580041398ff85901e5532e6c63fe0f63246a44
Von Christian <vc.halip@gmail.com>
2019-12-11 12:02:21 +0800

    fix null error

--- db/migrate/20191211022745_add_term_to_time_deposits.rb
+++ db/migrate/20191211022745_add_term_to_time_deposits.rb
@@ -1,5 +1,5 @@
 class AddTermToTimeDeposits < ActiveRecord::Migration[6.0]
   def change
-    add_reference :time_deposits, :term, null: false, foreign_key: true, type: :uuid 
+    add_reference :time_deposits, :term, foreign_key: true, type: :uuid 
   end
 end
