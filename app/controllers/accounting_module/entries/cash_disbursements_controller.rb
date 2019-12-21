09786f6de5c139dbee86ca6b808d45969c130bb9
Von Christian <vc.halip@gmail.com>
2019-12-21 10:19:46 +0800

    fix cash disbursements controller

--- app/controllers/accounting_module/entries/cash_disbursements_controller.rb
+++ app/controllers/accounting_module/entries/cash_disbursements_controller.rb
@@ -2,20 +2,19 @@ module AccountingModule
   module Entries
     class CashDisbursementsController < ApplicationController
       def index
-        @cooperative_service = params[:cooperative_service_id].present? ? current_cooperative.cooperative_services.find(params[:cooperative_service_id]) : nil
         if params[:from_date].present? && params[:to_date].present?
-          @from_date = params[:from_date] ? DateTime.parse(params[:from_date]) : Date.current.beginning_of_year
-          @to_date = params[:to_date] ? DateTime.parse(params[:to_date]) : Date.today.end_of_year
+          @from_date       = params[:from_date] ? DateTime.parse(params[:from_date]) : Date.current.beginning_of_year
+          @to_date         = params[:to_date] ? DateTime.parse(params[:to_date]) : Date.today.end_of_year
           @entries_for_pdf = current_office.cash_accounts.credit_entries.entered_on(from_date: @from_date, to_date: @to_date)
-          @ordered_entries = current_office.cash_accounts.credit_entries.order(reference_number: :desc).where(cooperative_service_id: params[:cooperative_service_id]).entered_on(from_date: @from_date, to_date: @to_date)
-          @entries = @ordered_entries.paginate(:page => params[:page], :per_page => 50)
+          @ordered_entries = current_office.cash_accounts.credit_entries.order(reference_number: :desc).entered_on(from_date: @from_date, to_date: @to_date)
+          @entries         = @ordered_entries.paginate(:page => params[:page], :per_page => 50)
 
         elsif params[:organization_id].present? && params[:search].present?
-          @organization = current_cooperative.organizations.find(params[:organization_id])
+          @organization    = current_cooperative.organizations.find(params[:organization_id])
           @entries_for_pdf = @organization.member_entries.text_search(params[:search])
           if @entries_for_pdf.present?
-            @from_date = @organization.member_entries.not_cancelled.order(entry_date: :asc).first.entry_date
-            @to_date = @organization.member_entries.not_cancelled.order(entry_date: :desc).first.entry_date
+            @from_date     = @organization.member_entries.not_cancelled.order(entry_date: :asc).first.entry_date
+            @to_date       = @organization.member_entries.not_cancelled.order(entry_date: :desc).first.entry_date
           end
           @ordered_entries = @organization.member_entries.order(reference_number: :desc).text_search(params[:search])
           @entries = @ordered_entries.paginate(:page => params[:page], :per_page => 50)
@@ -40,10 +39,10 @@ module AccountingModule
           @entries = @ordered_entries.paginate(:page => params[:page], :per_page => 50)
         
         else
-          @from_date = params[:from_date] ? DateTime.parse(params[:from_date]) : current_cooperative.entries.order(entry_date: :asc).first.entry_date
+          @from_date = params[:from_date] ? DateTime.parse(params[:from_date]) : Date.current.beginning_of_year
           @to_date = params[:to_date] ? DateTime.parse(params[:to_date]) : Date.today.end_of_year
-          @entries_for_pdf = current_cooperative.cash_accounts.credit_entries.where(cooperative_service_id: params[:cooperative_service_id])
-          @ordered_entries = current_cooperative.cash_accounts.credit_entries.order(reference_number: :desc).where(cooperative_service_id: params[:cooperative_service_id])
+          @entries_for_pdf = current_office.cash_accounts.credit_entries.entered_on(from_date: @from_date, to_date: @to_date).order(entry_date: :desc)
+          @ordered_entries = current_office.cash_accounts.credit_entries.order(reference_number: :desc).where(cooperative_service_id: params[:cooperative_service_id])
           @entries = @ordered_entries.paginate(page: params[:page], per_page:  50)
         end
         respond_to do |format|
