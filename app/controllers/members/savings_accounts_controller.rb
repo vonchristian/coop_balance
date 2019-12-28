5ba0030876cd29951a36a0f1eede7d44fff322be
Von Christian <vc.halip@gmail.com>
2019-12-28 19:31:10 +0800

    Fix savings account application system spec

--- app/controllers/members/savings_accounts_controller.rb
+++ app/controllers/members/savings_accounts_controller.rb
@@ -1,7 +1,7 @@
 module Members
   class SavingsAccountsController < ApplicationController
     def index
-      @member = current_cooperative.member_memberships.find(params[:member_id])
+      @member = current_office.member_memberships.find(params[:member_id])
       @savings = @member.savings
     end
   end
--- spec/system/members/savings_account_applications/new_system_spec.rb
+++ spec/system/members/savings_account_applications/new_system_spec.rb
@@ -3,34 +3,35 @@ include ChosenSelect
 
 describe 'New savings account application' do
   before(:each) do
-    cooperative = create(:cooperative)
-
-    cash_account = create(:asset, name: "Cash on Hand")
-    user = create(:user, role: 'teller', cooperative: cooperative)
-    user.cash_accounts << cash_account
+    cooperative    = create(:cooperative)
+    cash_account   = create(:asset, name: "Cash on Hand")
+    teller         = create(:teller, cooperative: cooperative)
+    member         = create(:member)
     saving_product = create(:saving_product, name: "Regular Savings", cooperative: cooperative)
-    member = create(:member)
-    member.memberships.create!(cooperative: cooperative, membership_type: 'regular_member', account_number: SecureRandom.uuid)
-    login_as(user, scope: :user)
-    visit member_savings_accounts_url(member)
-    click_link "New Saving"
+    member.memberships.create!(cooperative: cooperative, membership_type: 'regular_member', account_number: SecureRandom.uuid, office: teller.office)
+    teller.cash_accounts << cash_account
+    create(:office_saving_product, office: teller.office, saving_product: saving_product)
+    
+    login_as(teller, scope: :user)
+    visit member_savings_accounts_path(member)
+    click_link "New Savings Account"
   end
 
-  it "with valid attributes" do
-    fill_in "Date", with: Date.current.strftime('%B %e, %Y')
-    select "Regular Savings", from: 'Saving product'
-    fill_in "Amount", with: 5_000
-    fill_in "Description", with: "Opening of savings account"
-    fill_in "Beneficiaries", with: "Beneficiary"
+  it "with valid attributes", js: true do
+    select_from_chosen "Regular Savings", from: 'Saving product'
+    fill_in "Date",             with: Date.current.strftime('%B %e, %Y')
+    fill_in "Amount",           with: 5_000
+    fill_in "Description",      with: "Opening of savings account"
+    fill_in "Beneficiaries",    with: "Beneficiary"
     fill_in "Reference number", with: "909090"
-    select "Cash on Hand"
+    select_from_chosen "Cash on Hand", from: 'Cash Account'
 
     click_button "Proceed"
 
     expect(page).to have_content "created successfully"
 
 
-    click_link "Confirm"
+    click_link "Confirm Transaction"
 
     expect(page).to have_content "opened successfully"
 
