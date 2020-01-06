e930142fff306d6f5be94d074bf7dc046b7ece03
Von Christian <vc.halip@gmail.com>
2020-01-06 11:57:17 +0800

    Fix membership application system spec

--- spec/system/membership_applications/new_membership_application_system_spec.rb
+++ spec/system/membership_applications/new_membership_application_system_spec.rb
@@ -3,41 +3,46 @@ include ChosenSelect
 
 describe "New membership application" do
   before(:each) do
-   user                = create(:user, role: 'teller')
-   province            = create(:province, name: "Test province")
-   municipality        = create(:municipality, name: "Test municipality", province: province)
-   barangay            = create(:barangay, name: "Test barangay", municipality: municipality)
-   
+    user                = create(:user, role: 'teller')
+    province            = create(:province, name: "Test province")
+    municipality        = create(:municipality, name: "Test municipality", province: province)
+    barangay            = create(:barangay, name: "Test barangay", municipality: municipality)
     cooperative         = user.cooperative
     membership_category = create(:membership_category, cooperative: cooperative, title: 'Regular Member')
     office              = user.office
-    
+      
     login_as(user, scope: :user)
     visit members_path
     find(:css, 'i.fa.fa-user-plus').click
   end
 
-  it "wtih valid attributes", js: true do
-    select_from_chosen "Regular Member", from: 'Membership category'
-    fill_in "First name",       with: "Von"
-    fill_in "Middle name",      with: "P"
-    fill_in "Last name",        with: "Halip"
-    choose "Male"
-    choose "Married"
-    fill_in "Date of birth",    with: "02/12/1990"
-    fill_in "Contact number",   with: "48234239482934"
-    fill_in "Email",            with: 'ddd@example.com'
-    fill_in "TIN Number",       with: "2342424"
-    fill_in "Complete address", with: "dasjd aksdaj skda"
+  it "with valid attributes", js: true do
+    select_from_chosen "Regular Member",   from: 'Membership category'
+    fill_in "First name",                   with: "Von"
+    fill_in "Middle name",                  with: "P"
+    fill_in "Last name",                    with: "Halip"
+    choose "Male"            
+    choose "Married"            
+    fill_in "Date of birth",                with: "02/12/1990"
+    fill_in "Contact number",               with: "48234239482934"
+    fill_in "Email",                        with: 'ddd@example.com'
+    fill_in "TIN Number",                   with: "2342424"
+    fill_in "Complete address",             with: "dasjd aksdaj skda"
     page.execute_script "window.scrollBy(0,10000)"
     select_from_chosen "Test province",     from: 'Province'
     select_from_chosen "Test municipality", from: 'Municipality'
     select_from_chosen "Test barangay",     from: 'Barangay'
-
-    fill_in "Membership date",  with: Date.current
+    fill_in "Membership date",               with: Date.current
 
     click_button "Save Member"
 
     expect(page).to have_content("saved successfully")
   end
+
+  it 'with invalid attributes', js: true do 
+    
+    click_button 'Save Member'
+
+    expect(page).to have_content("can't be blank")
+  end 
 end
