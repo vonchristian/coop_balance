a2b4cab89cffeba0b08742c49407dc3595fbe5cf
Von Christian <vc.halip@gmail.com>
2020-01-07 08:13:21 +0800

    updates

---
+++ spec/system/members/settings/index_system_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+describe 'Member settings index page' do 
+  it 'with valid attributes' do 
+    member  = create(:member)
+    manager = create(:general_manager)
+    create(:membership, cooperator: member, cooperative: manager.cooperative)
+    login_as(manager, scope: :user)
+    visit member_path(member)
+    click_link "#{member.id}-settings"
+
+    expect(page).to have_content 'Member Settings'
+  end
+
+end 
