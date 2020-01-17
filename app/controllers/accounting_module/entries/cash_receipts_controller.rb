fc5dddb1f43728432f838eeb87859b31170e39ef
Von Christian <vc.halip@gmail.com>
2020-01-17 08:32:32 +0800

    update cash pdf report

--- app/controllers/accounting_module/entries/cash_receipts_controller.rb
+++ app/controllers/accounting_module/entries/cash_receipts_controller.rb
@@ -4,10 +4,10 @@ module AccountingModule
       def index
         @cooperative_service = current_cooperative.cooperative_services.find(params[:cooperative_service_id]) if params[:cooperative_service_id].present?
         if params[:from_date].present? && params[:to_date].present?
-          @from_date = params[:from_date] ? DateTime.parse(params[:from_date]) : current_cooperative.entries.order(entry_date: :asc).first.entry_date
+          @from_date = params[:from_date] ? DateTime.parse(params[:from_date]) : current_office.entries.order(entry_date: :asc).first.entry_date
           @to_date = params[:to_date] ? DateTime.parse(params[:to_date]) : Date.today.end_of_year
-          @entries_for_pdf = current_cooperative.cash_accounts.debit_entries.where(cooperative_service_id: params[:cooperative_service_id]).entered_on(from_date: @from_date, to_date: @to_date)
-          @ordered_entries = current_cooperative.cash_accounts.debit_entries.order(reference_number: :desc).where(cooperative_service_id: params[:cooperative_service_id]).entered_on(from_date: @from_date, to_date: @to_date)
+          @entries_for_pdf = current_office.cash_accounts.debit_entries.where(cooperative_service_id: params[:cooperative_service_id]).entered_on(from_date: @from_date, to_date: @to_date)
+          @ordered_entries = current_office.cash_accounts.debit_entries.order(reference_number: :desc).where(cooperative_service_id: params[:cooperative_service_id]).entered_on(from_date: @from_date, to_date: @to_date)
           @entries = @ordered_entries.paginate(:page => params[:page], :per_page => 50)
 
         elsif params[:organization_id].present? && params[:search].present?
@@ -42,8 +42,8 @@ module AccountingModule
         else
           @from_date = params[:from_date] ? DateTime.parse(params[:from_date]) : current_cooperative.entries.order(entry_date: :asc).first.entry_date
           @to_date = params[:to_date] ? DateTime.parse(params[:to_date]) : Date.today.end_of_year
-          @entries_for_pdf = current_cooperative.cash_accounts.debit_entries.where(cooperative_service_id: params[:cooperative_service_id])
-          @ordered_entries = current_cooperative.cash_accounts.debit_entries.order(reference_number: :desc).where(cooperative_service_id: params[:cooperative_service_id])
+          @entries_for_pdf = current_office.cash_accounts.debit_entries.where(cooperative_service_id: params[:cooperative_service_id])
+          @ordered_entries = current_office.cash_accounts.debit_entries.order(reference_number: :desc).where(cooperative_service_id: params[:cooperative_service_id])
           @entries = @ordered_entries.paginate(page: params[:page], per_page:  50)
         end
         respond_to do |format|
--- config/puma.rb
+++ config/puma.rb
@@ -1,19 +1,19 @@
-workers Integer(ENV['WEB_CONCURRENCY'] || 2)
-threads_count = Integer(ENV['RAILS_MAX_THREADS'] || 5)
-threads threads_count, threads_count
+# workers Integer(ENV['WEB_CONCURRENCY'] || 2)
+# threads_count = Integer(ENV['RAILS_MAX_THREADS'] || 5)
+# threads threads_count, threads_count
 
-environment 'production'
-directory '/var/www/rbg/current'
-shared_dir = File.expand_path("../..", __FILE__)
+# environment 'production'
+# directory '/var/www/rbg/current'
+# shared_dir = File.expand_path("../..", __FILE__)
 
-stdout_redirect "#{shared_dir}/log/puma.stdout.log", "#{shared_dir}/log/puma.stderr.log", true
+# stdout_redirect "#{shared_dir}/log/puma.stdout.log", "#{shared_dir}/log/puma.stderr.log", true
 
-rackup DefaultRackup
-bind "unix://#{shared_dir}/tmp/sockets/puma.sock"
-pidfile "#{shared_dir}/tmp/pids/puma.pid"
+# rackup DefaultRackup
+# bind "unix://#{shared_dir}/tmp/sockets/puma.sock"
+# pidfile "#{shared_dir}/tmp/pids/puma.pid"
 
-prune_bundler
-preload_app!
-on_worker_boot do
-  ActiveRecord::Base.establish_connection
-end
+# prune_bundler
+# preload_app!
+# on_worker_boot do
+#   ActiveRecord::Base.establish_connection
+# end
