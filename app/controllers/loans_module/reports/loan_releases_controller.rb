757d80874959567f8b04c5a94995e94eccefd4ae
Von Christian <vc.halip@gmail.com>
2020-01-22 16:08:49 +0800

    fix loan releases report

--- app/controllers/loans_module/reports/loan_releases_controller.rb
+++ app/controllers/loans_module/reports/loan_releases_controller.rb
@@ -9,6 +9,7 @@ module LoansModule
         respond_to do |format|
           format.html
           format.xlsx
+          format.csv { render_csv }
           format.pdf do
             pdf = LoansModule::Reports::LoanReleasesPdf.new(
               loans: @loans,
@@ -20,6 +21,52 @@ module LoansModule
           end
         end
       end
-    end
-  end
+
+      private 
+
+		def render_csv
+			# Tell Rack to stream the content
+			headers.delete("Content-Length")
+
+			# Don't cache anything from this generated endpoint
+			headers["Cache-Control"] = "no-cache"
+
+			# Tell the browser this is a CSV file
+			headers["Content-Type"] = "text/csv"
+
+			# Make the file download with a specific filename
+			headers["Content-Disposition"] = "attachment; filename=\"Loans Portfolio.csv\""
+
+			# Don't buffer when going through proxy servers
+			headers["X-Accel-Buffering"] = "no"
+
+			# Set an Enumerator as the body
+			self.response_body = csv_body
+
+			response.status = 200
+		end
+
+		private
+
+		def csv_body
+			Enumerator.new do |yielder|
+				yielder << CSV.generate_line(["#{current_office.name} Loan Disbursements"])
+				yielder << CSV.generate_line(["Borrower", "Loan Product", "Loan Purpose", "Loan Amount", "Principal Balance", 'Interests', 'Penalties',  "Disbursement Date", "Maturity Date"])
+				@loans.each do |loan|
+					yielder << CSV.generate_line([
+						loan.borrower_full_name,
+						loan.loan_product_name,
+            loan.purpose,
+            loan.loan_amount,
+						loan.principal_balance(to_date: @to_date),
+						loan.loan_interests_balance,
+						loan.loan_penalties_balance,
+						loan.disbursement_date.try(:strftime, ("%B %e, %Y")),
+						loan.maturity_date.try(:strftime,('%B %e, %Y'))
+            ])
+          end 
+				end
+			end 
+		end 
+	end
 end
