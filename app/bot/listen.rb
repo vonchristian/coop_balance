2c483b04bab703ad5fe0db0cd1ad4218601fe529
Von Christian <vc.halip@gmail.com>
2020-05-26 08:28:48 +0800

    Fix zeitwerk error

--- app/bot/listen.rb
+++
@@ -1,8 +0,0 @@
-# require "facebook/messenger"
-
-# include Facebook::Messenger
-
-# Facebook::Messenger::Subscriptions.subscribe(access_token: 'EAAEUD40TNPIBALeilpZAEVQMfOOD4nXE6QwbZA1K8F1ZB9o0M8aA7OubDZAZAmrFdnKsxfZBnZBplpukhCBp6xk44s7oHnmVZBXz3OYgvIrha8MopZCZCo5pq6DQwVschBsmurFO61XMMaKPIyoXwV0X0RKWTNgaWpgDrdKcrBajb3hKX1zLdrQHfp') 
-
-# Bot.on :message do |message|
-#   puts "got your message!"
