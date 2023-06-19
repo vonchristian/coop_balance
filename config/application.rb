c4fbbb284561586c9f8897dcf141cfbf1cdf0c14
Von Christian Halip <vc.halip@gmail.com>
2023-06-19 23:33:37 +0800

    Test fix

--- config/application.rb
+++ config/application.rb
@@ -17,7 +17,8 @@ module CoopBalance
     config.beginning_of_week = :sunday
     config.autoload_paths << Rails.root.join('workers')
     config.autoload_paths << Rails.root.join('bot')
+    config.active_record.yaml_column_permitted_classes = [BigDecimal]
   end
 end
 Rails.autoloaders.main.ignore(Rails.root.join('app/node_modules'))
-ActiveRecord.yaml_column_permitted_classes = [BigDecimal]
+
