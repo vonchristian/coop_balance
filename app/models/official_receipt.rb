bd36ed1de774ccbd92c10836f60529c082d71ccb
Von Christian Halip <vc.halip@gmail.com>
2023-08-21 15:18:04 +0800

    Delete official receipt

--- app/models/official_receipt.rb
+++
@@ -1,7 +0,0 @@
-class OfficialReceipt < ApplicationRecord
-  belongs_to :receiptable, polymorphic: true
-  def self.generate_number
-    return self.create(number: "1".rjust(15, "0")) if all.blank?
-    self.create(number: all.order(created_at: :asc).last.number.succ.rjust(15, "0"))
-  end
-end
