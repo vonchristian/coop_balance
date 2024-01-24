9c70179bf557c28883945f1471df3ed5d074c74e
Von Christian Halip <vc.halip@gmail.com>
2024-01-24 11:42:44 +0800

    Fixes

--- app/forms/vouchers/voucher_processing.rb
+++ app/forms/vouchers/voucher_processing.rb
@@ -38,7 +38,7 @@ module Vouchers
     end
 
     def find_payee
-      Payee.find_by_id(id: payee_id)
+      Payee.find_by_id(payee_id) # rubocop:disable Rails/DynamicFindBy
     end
 
     def find_cart
