e2f7ced30099316f29a2df04e9a0b5c8ed58fcaf
Von Christian Halip <vc.halip@gmail.com>
2025-03-01 09:16:27 +0800

    nokogiri

--- config/deploy.rb
+++ config/deploy.rb
@@ -29,6 +29,7 @@ task deploy: :remote_environment do
     invoke :'deploy:link_shared_paths'
     invoke :'bundle:install'
     invoke :'rails:db_migrate'
+    command "gem pristine nokogiri"
     invoke :'rails:assets_precompile'
     invoke :'deploy:cleanup'
 
