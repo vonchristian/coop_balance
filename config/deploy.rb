e13df8aa1b8051d7be442a54e402ed3696a16103
Von Christian Halip <vc.halip@gmail.com>
2025-03-01 08:36:16 +0800

    Bump bundler

--- config/deploy.rb
+++ config/deploy.rb
@@ -29,8 +29,7 @@ task deploy: :remote_environment do
     invoke :'deploy:link_shared_paths'
     invoke :'bundle:install'
     invoke :'rails:db_migrate'
-    command 'bundle config set force_ruby_platform true'
-    # invoke :'rails:assets_precompile'
+    invoke :'rails:assets_precompile'
     invoke :'deploy:cleanup'
 
     on :launch do
